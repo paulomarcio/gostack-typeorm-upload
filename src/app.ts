ecadac4ae8752fa6e3900ac9274b597c26b1c793
Paulo Márcio Matias da Silva <paulomarciomatiasdasilva@MacBook-Pro-de-Paulo.local>
2020-05-21 19:16:03 -0300

    Added cors library

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import 'dotenv/config';
+import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
@@ -13,6 +14,7 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
@@ -22,8 +24,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
       message: err.message,
     });
   }
-
-  console.error(err);
 
   return response.status(500).json({
     status: 'error',
